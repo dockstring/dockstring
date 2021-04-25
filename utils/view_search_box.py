dd244d7902ec5495d0dadbae370aacd85cfa71a7
mgarort <miwe.gar.or@gmail.com>
2021-04-25 02:20:14 +0100

    Define view_search_box function for Pymol

---
+++ utils/view_search_box.py
@@ -0,0 +1,78 @@
+# Inspired by drawBoundingBox.py by JasonVertrees
+
+from pymol.cgo import *
+from pymol import cmd
+
+def view_search_box(center_x, center_y, center_z, size_x, size_y, size_z):
+    linewidth = 2.0
+    r = 1.0
+    g = 1.0
+    b = 1.0
+    center_x = float(center_x)
+    center_y = float(center_y)
+    center_z = float(center_z)
+    size_x   = float(size_x)
+    size_y   = float(size_y)
+    size_z   = float(size_z)
+
+
+    # Vina adds a 5Å padding (TODO check this is exactly true)
+    min_x = center_x - size_x/2 - 5
+    min_y = center_y - size_y/2 - 5
+    min_z = center_z - size_z/2 - 5
+    max_x = center_x + size_x/2 + 5
+    max_y = center_y + size_y/2 + 5
+    max_z = center_z + size_z/2 + 5
+
+    # Define the search box
+    search_box = [
+            LINEWIDTH, float(linewidth),
+
+            BEGIN, LINES,
+            COLOR, float(r), float(g), float(b),
+
+            VERTEX, min_x, min_y, min_z,       #1
+            VERTEX, min_x, min_y, max_z,       #2
+
+            VERTEX, min_x, max_y, min_z,       #3
+            VERTEX, min_x, max_y, max_z,       #4
+
+            VERTEX, max_x, min_y, min_z,       #5
+            VERTEX, max_x, min_y, max_z,       #6
+
+            VERTEX, max_x, max_y, min_z,       #7
+            VERTEX, max_x, max_y, max_z,       #8
+
+
+            VERTEX, min_x, min_y, min_z,       #1
+            VERTEX, max_x, min_y, min_z,       #5
+
+            VERTEX, min_x, max_y, min_z,       #3
+            VERTEX, max_x, max_y, min_z,       #7
+
+            VERTEX, min_x, max_y, max_z,       #4
+            VERTEX, max_x, max_y, max_z,       #8
+
+            VERTEX, min_x, min_y, max_z,       #2
+            VERTEX, max_x, min_y, max_z,       #6
+
+
+            VERTEX, min_x, min_y, min_z,       #1
+            VERTEX, min_x, max_y, min_z,       #3
+
+            VERTEX, max_x, min_y, min_z,       #5
+            VERTEX, max_x, max_y, min_z,       #7
+
+            VERTEX, min_x, min_y, max_z,       #2
+            VERTEX, min_x, max_y, max_z,       #4
+
+            VERTEX, max_x, min_y, max_z,       #6
+            VERTEX, max_x, max_y, max_z,       #8
+
+            END
+    ]
+
+    cmd.load_cgo(search_box, 'search_box')
+    return 'search_box'
+
+cmd.extend('view_search_box', view_search_box)
