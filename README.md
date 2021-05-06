02fbf59ad899df7942702fa958d3c6315880acdc
Gregor Simm <gncsimm@gmail.com>
2021-05-06 14:02:15 +0100

    add and apply YAPF formatting

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ A docking benchmark for ML applications. (TODO improve description)
 conda create -n dockgym
 conda activate dockgym
 conda install -c conda-forge pymol-open-source rdkit openbabel
+```
+
+# Development
+
+## YAPF
+
+```bash
+yapf --style=.style.yapf --in-place --recursive ./src
+```
--- src/dockgym.py
+++ src/dockgym.py
@@ -9,11 +9,13 @@ from rdkit.Chem import AllChem as Chem
 from rdkit import rdBase
 from .utils import DockingError, get_num_conf
 
+
 def load_target(name):
     return Target(name)
 
+
 class Target():
-    def __init__(self,name,random_seed=974528263):
+    def __init__(self, name, random_seed=974528263):
         self._name = name
         self.random_seed = random_seed
         # TODO Determine whether the OS is Linux, Mac or Windows
@@ -34,12 +36,12 @@ class Target():
         self._tmp_dir_handle = tempfile.TemporaryDirectory()
         # Copy PDB, PDBQT and conf files to the temporary directory
         # TODO Create PDBQT for receptors
-        pdb_reference = self._receptors_dir  / (self.name + '_receptor.pdb')
-        shutil.copyfile(pdb_reference,self._pdb)
+        pdb_reference = self._receptors_dir / (self.name + '_receptor.pdb')
+        shutil.copyfile(pdb_reference, self._pdb)
         pdbqt_reference = self._receptors_dir / (self.name + '_receptor.pdbqt')
-        shutil.copyfile(pdbqt_reference,self._pdbqt)
+        shutil.copyfile(pdbqt_reference, self._pdbqt)
         conf_reference = self._receptors_dir / (self.name + '_conf.txt')
-        shutil.copyfile(conf_reference,self._conf)
+        shutil.copyfile(conf_reference, self._conf)
 
     def __del__(self):
         self._tmp_dir_handle.cleanup()
@@ -52,27 +54,36 @@ class Target():
     @property
     def _tmp_dir(self):
         return Path(self._tmp_dir_handle.name).resolve()
+
     @property
     def _receptors_dir(self):
-        return Path(sys.modules[self.__class__.__module__].__file__).parent.parent / 'receptors'
+        return Path(sys.modules[
+            self.__class__.__module__].__file__).parent.parent / 'receptors'
+
     @property
     def _bin_dir(self):
-        return Path(sys.modules[self.__class__.__module__].__file__).parent.parent / 'bin'
+        return Path(sys.modules[
+            self.__class__.__module__].__file__).parent.parent / 'bin'
+
     @property
     def _bin_dir(self):
-        return Path(sys.modules[self.__class__.__module__].__file__).parent.parent / 'bin'
+        return Path(sys.modules[
+            self.__class__.__module__].__file__).parent.parent / 'bin'
+
     @property
     def _pdb(self):
         return Path(self._tmp_dir / (self.name + '_receptor.pdb'))
+
     @property
     def _pdbqt(self):
         return Path(self._tmp_dir / (self.name + '_receptor.pdbqt'))
+
     @property
     def _conf(self):
         return Path(self._tmp_dir / (self.name + '_conf.txt'))
 
     # Submethods for docking a ligand
-    def _smiles_or_inchi_2_mol(self,smiles_or_inchi):
+    def _smiles_or_inchi_2_mol(self, smiles_or_inchi):
         rdBase.DisableLog('rdApp.error')
         mol = Chem.MolFromSmiles(smiles_or_inchi)
         if mol is None:
@@ -83,18 +94,18 @@ class Target():
         rdBase.EnableLog('rdApp.error')
         return mol
 
-
-    def _mol_2_embedding(self,mol):
+    def _mol_2_embedding(self, mol):
         # Will attempt to find 3D coordinates 10 times with different random seeds
         n_attempts = 10
         # Set first random seed for reproducibility
         random_seed = self._dock_random_seed
         for i in range(n_attempts):
             # Simple approach to get subsequent random seeds from first random seed
-            random_seed = (random_seed - (-1)**(i % 2) * i * random_seed) % 766523564
+            random_seed = (random_seed -
+                           (-1)**(i % 2) * i * random_seed) % 766523564
             # Always add hydrogens in order to get a sensible 3D structure, and remove them later
             mol = Chem.AddHs(mol)
-            Chem.EmbedMolecule(mol,randomSeed=random_seed)
+            Chem.EmbedMolecule(mol, randomSeed=random_seed)
             mol = Chem.RemoveHs(mol)
             # If at least one conformation has been obtained, don't try more and break out of the loop.
             # Otherwise, keep trying to hopefully generate a ligand conformation
@@ -108,12 +119,17 @@ class Target():
         else:
             return mol
 
-    def _embedding_2_pdb(self,mol,ligand_pdb):
-        Chem.MolToPDBFile(mol,str(ligand_pdb))
+    def _embedding_2_pdb(self, mol, ligand_pdb):
+        Chem.MolToPDBFile(mol, str(ligand_pdb))
 
-    def _pdb_2_pdbqt(self,ligand_pdb,ligand_pdbqt):
-        cmd_list = ['obabel', '-ipdb', ligand_pdb, '-opdbqt', '-O', ligand_pdbqt, '--partialcharge', 'gasteiger']
-        cmd_return = subprocess.run(cmd_list, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    def _pdb_2_pdbqt(self, ligand_pdb, ligand_pdbqt):
+        cmd_list = [
+            'obabel', '-ipdb', ligand_pdb, '-opdbqt', '-O', ligand_pdbqt,
+            '--partialcharge', 'gasteiger'
+        ]
+        cmd_return = subprocess.run(cmd_list,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.STDOUT)
         output = cmd_return.stdout.decode('utf-8')
         # If there exists a logfile handle, save output there. If not, do nothing
         try:
@@ -128,16 +144,26 @@ class Target():
             raise DockingError(f'Docking of molecule  {self._mol_id}  failed during the ' \
                     'conversion of PDB to PDBQT with OpenBabel.')
 
-    def _dock_pdbqt(self,ligand_pdbqt,vina_logfile,vina_outfile,num_cpu=None):
+    def _dock_pdbqt(self,
+                    ligand_pdbqt,
+                    vina_logfile,
+                    vina_outfile,
+                    num_cpu=None):
         if num_cpu is None:
             cpu_argument = ''
         else:
             cpu_argument = '--cpu {num_cpu}'
-        cmd_list = [f'{self._vina}', '--receptor', self._pdbqt, '--config', self._conf, '--ligand', ligand_pdbqt,
-                    '--log', vina_logfile, '--out', vina_outfile, '--seed', str(self._dock_random_seed)]
+        cmd_list = [
+            f'{self._vina}', '--receptor', self._pdbqt, '--config', self._conf,
+            '--ligand', ligand_pdbqt, '--log', vina_logfile, '--out',
+            vina_outfile, '--seed',
+            str(self._dock_random_seed)
+        ]
         if num_cpu is not None:
             cmd_list += ['--cpu', str(num_cpu)]
-        cmd_return = subprocess.run(cmd_list, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        cmd_return = subprocess.run(cmd_list,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.STDOUT)
         output = cmd_return.stdout.decode('utf-8')
         # If there exists a logfile handle, save output there. If not, do nothing
         try:
@@ -152,7 +178,7 @@ class Target():
             raise DockingError(f'Docking of molecule  {self._mol_id}  failed during the ' \
                     'docking with Vina.')
 
-    def _get_top_score_from_vina_logfile(self,vina_logfile):
+    def _get_top_score_from_vina_logfile(self, vina_logfile):
         with open(vina_logfile, 'r') as f:
             counter_to_score = None
             for each_line in f:
@@ -167,8 +193,7 @@ class Target():
         top_score = float(line_with_score.split()[1])
         return top_score
 
-
-    def dock(self,mol,num_cpu=None,seed=None,logfile=None,verbose=False):
+    def dock(self, mol, num_cpu=None, seed=None, logfile=None, verbose=False):
         '''
         Given a molecule, this method will return a docking score against the current target.
         - mol: either a SMILES string, an inchikey or a RDKit molecule object
@@ -184,7 +209,7 @@ class Target():
         5. Extract all the info from the docking output
         '''
         # Define molecule identifier for error messages
-        if isinstance(mol,Chem.Mol):
+        if isinstance(mol, Chem.Mol):
             self._mol_id = Chem.MolToSmiles(mol)
         else:
             self._mol_id = mol
@@ -193,26 +218,29 @@ class Target():
         else:
             self._dock_random_seed = seed
         if logfile is not None:
-            self._dock_logfile_handle = open(logfile,'w')
+            self._dock_logfile_handle = open(logfile, 'w')
         self._dock_verbose = verbose
         # Docking with Vina is performed in a temporary directory
         with tempfile.TemporaryDirectory() as dock_tmp_dir:
             # Define necessary filenames
             dock_tmp_dir = Path(dock_tmp_dir)
-            ligand_pdb   = (dock_tmp_dir / 'ligand.pdb').resolve()
+            ligand_pdb = (dock_tmp_dir / 'ligand.pdb').resolve()
             ligand_pdbqt = (dock_tmp_dir / 'ligand.pdbqt').resolve()
             vina_logfile = (dock_tmp_dir / 'vina.log').resolve()
             vina_outfile = (dock_tmp_dir / 'vina.out').resolve()
             try:
                 # Prepare ligand
                 # TODO Handle RDKit output too with verbose/logfile
-                if not isinstance(mol,Chem.Mol):
+                if not isinstance(mol, Chem.Mol):
                     mol = self._smiles_or_inchi_2_mol(mol)
                 mol = self._mol_2_embedding(mol)
-                self._embedding_2_pdb(mol,ligand_pdb)
-                self._pdb_2_pdbqt(ligand_pdb,ligand_pdbqt)
+                self._embedding_2_pdb(mol, ligand_pdb)
+                self._pdb_2_pdbqt(ligand_pdb, ligand_pdbqt)
                 # Dock
-                self._dock_pdbqt(ligand_pdbqt,vina_logfile,vina_outfile,num_cpu=num_cpu)
+                self._dock_pdbqt(ligand_pdbqt,
+                                 vina_logfile,
+                                 vina_outfile,
+                                 num_cpu=num_cpu)
                 # Process docking output
                 score = self._get_top_score_from_vina_logfile(vina_logfile)
                 # TODO Get the pose from vina_outfile
@@ -223,7 +251,7 @@ class Target():
                     self._dock_logfile_handle.close()
                     del self._dock_logfile_handle
                 del self._dock_verbose
-                return (score,None)
+                return (score, None)
             except DockingError as error:
                 print(f'DockingError: ' + str(error))
                 del self._dock_random_seed
@@ -232,7 +260,6 @@ class Target():
                     del self._dock_logfile_handle
                 del self._dock_verbose
                 return (None, None)
-
 
             # Using tempfile makes the temporary file creation portable for all systems
             print(dock_tmp_dir)
@@ -254,35 +281,33 @@ class Target():
         '''
         Start pymol and view the receptor and the search box.
         '''
-        with open(self._conf,'r') as f:
+        with open(self._conf, 'r') as f:
             # Extract the search box information
             for line in f:
-                if   'center_x' in line:
+                if 'center_x' in line:
                     center_x = float(line.split()[2])
                 elif 'center_y' in line:
                     center_y = float(line.split()[2])
                 elif 'center_z' in line:
                     center_z = float(line.split()[2])
-                elif 'size_x'   in line:
-                    size_x   = float(line.split()[2])
-                elif 'size_y'   in line:
-                    size_y   = float(line.split()[2])
-                elif 'size_z'   in line:
-                    size_z   = float(line.split()[2])
+                elif 'size_x' in line:
+                    size_x = float(line.split()[2])
+                elif 'size_y' in line:
+                    size_y = float(line.split()[2])
+                elif 'size_z' in line:
+                    size_z = float(line.split()[2])
 
             receptor = str(self._pdb.resolve())
             # TODO Remove ligand from the main view() method, make it a part of the docked ligands' method
             ligand = f'/home/mgarort/repos/dockgym/playground/crystal_ligands/{self.name}/crystal_ligand.mol2'
-            pymol_view_search_box = Path(sys.modules[self.__class__.__module__].__file__).parent.parent / 'utils' / 'view_search_box.py'
+            pymol_view_search_box = Path(
+                sys.modules[self.__class__.__module__].__file__
+            ).parent.parent / 'utils' / 'view_search_box.py'
             command = f'pymol -R {pymol_view_search_box} {receptor} {ligand}'
             if search_box:
                 command += f" -d 'view_search_box center_x={center_x}, center_y={center_y}, center_z={center_z}, \
                               size_x={size_x}, size_y={size_y}, size_z={size_z}'"
+
             command += ' > /dev/null'
             # TODO Change to subprocess
             os.system(command)
-
-
-
-
-
