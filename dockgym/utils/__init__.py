418c4d47873d9f15936dd3107f91c2a9b1919bf8
Gregor Simm <gncsimm@gmail.com>
2021-05-08 20:46:05 +0100

    remove get_num_confs function

--- dockgym/utils/__init__.py
+++ dockgym/utils/__init__.py
@@ -1,3 +1,3 @@
-from .utils import DockingError, get_num_conf
+from .utils import DockingError
 
-__all__ = ['DockingError', 'get_num_conf']
+__all__ = ['DockingError']
--- dockgym/utils/utils.py
+++ dockgym/utils/utils.py
@@ -1,13 +1,3 @@
-# Exceptions
-
-
 class DockingError(Exception):
     """Raised when Target.dock fails at any step"""
     pass
-
-
-# Chemistry
-
-
-def get_num_conf(mol):
-    return sum(1 for conformer in mol.GetConformers())
