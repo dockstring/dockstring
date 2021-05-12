8349cf57a606c0cddb5e3a3705b0964bc127eb49
Gregor Simm <gncsimm@gmail.com>
2021-05-12 10:00:12 +0100

    clean up view function

--- dockgym/view.py
+++ dockgym/view.py
@@ -15,16 +15,19 @@ def view(target: Target, mols: List[Chem.Mol] = None, search_box=True):
     """
     Start pymol and view the receptor and the search box.
     """
-    pymol_view_search_box_file = pkg_resources.resource_filename(__package__,
-                                                                 os.path.join('utils', 'view_search_box.py'))
-    commands = ['pymol', pymol_view_search_box_file, target._pdb]
+    commands = ['pymol', target._pdb]
 
     if search_box:
+        pymol_view_search_box_file = pkg_resources.resource_filename(__package__,
+                                                                     os.path.join('utils', 'view_search_box.py'))
         conf = parse_search_box_conf(target._conf)
+        # yapf: disable
         commands += [
+            pymol_view_search_box_file,
             '-d', 'view_search_box center_x={center_x}, center_y={center_y}, center_z={center_z}, '
             'size_x={size_x}, size_y={size_y}, size_z={size_z}'.format(**conf)
         ]
+        # yapf: enable
 
     if mols:
         tmp_dir_handle = tempfile.TemporaryDirectory()
