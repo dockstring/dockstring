a02a2ede8bd9dc060d7d8d6fb41d8bfdfc09bee1
Gregor Simm <gncsimm@gmail.com>
2021-05-12 09:38:36 +0100

    remove unused import statement

--- dockgym/dockgym.py
+++ dockgym/dockgym.py
@@ -10,7 +10,7 @@ from rdkit.Chem import AllChem as Chem
 
 from dockgym.utils import (DockingError, get_vina_filename, smiles_or_inchi_to_mol, embed_mol,
                            write_embedded_mol_to_pdb, convert_pdbqt_to_pdb, convert_pdb_to_pdbqt, read_pdb_to_mol,
-                           parse_scores_from_pdb, parse_search_box_conf)
+                           parse_scores_from_pdb)
 
 
 def get_targets_dir() -> Path:
