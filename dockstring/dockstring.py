de76a06cd564d6546960b886f908d0769bfe4718
Miguel Garcia-Ortegon <mg770@cam.ac.uk>
2021-05-17 02:56:34 +0100

    Format with yapf and lint with flake8

--- dockstring/dockstring.py
+++ dockstring/dockstring.py
@@ -9,11 +9,9 @@ from typing import Optional, List
 import pkg_resources
 from rdkit.Chem import AllChem as Chem
 
-
 from dockstring.utils import (DockingError, get_vina_filename, smiles_or_inchi_to_mol, embed_mol, refine_mol_with_ff,
                               write_embedded_mol_to_pdb, protonate_pdb, convert_pdbqt_to_pdb, convert_pdb_to_pdbqt,
                               read_mol_from_pdb, parse_scores_from_pdb, parse_search_box_conf, PathType)
-
 
 logging.basicConfig(format='%(message)s')
 
