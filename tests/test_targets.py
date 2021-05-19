f9ec6664d3f16b1f17da1726b186784e7ac9ff26
Gregor Simm <gncsimm@gmail.com>
2021-05-19 19:14:47 +0100

    add more tests

--- tests/test_targets.py
+++ tests/test_targets.py
@@ -1,8 +1,12 @@
 import math
+import tempfile
 
 import pytest
+import rdkit.Chem as Chem
 
 from dockstring import list_all_target_names, load_target, DockingError
+from dockstring.utils import (smiles_or_inchi_to_mol, write_embedded_mol_to_pdb, embed_mol, read_mol_from_pdb,
+                              protonate_pdb, convert_pdb_to_pdbqt, convert_pdbqt_to_pdb)
 
 
 class TestLoader:
@@ -17,6 +21,64 @@ class TestLoader:
             load_target('does_not_exist')
 
 
+lysine_smiles = 'C(CCN)C[C@@H](C(=O)O)N'
+
+
+class TestConversions:
+    def test_convert_string_success(self):
+        assert smiles_or_inchi_to_mol('C')
+
+    def test_convert_string_fail(self):
+        with pytest.raises(DockingError):
+            smiles_or_inchi_to_mol('not_a_mol')
+
+    def test_read_fail(self):
+        mol = smiles_or_inchi_to_mol(lysine_smiles)
+        with tempfile.NamedTemporaryFile(suffix='.pdb') as f:
+            with pytest.raises(DockingError):
+                write_embedded_mol_to_pdb(mol, ligand_pdb=f.name)
+
+    def test_read_write_pdb(self):
+        mol = smiles_or_inchi_to_mol(lysine_smiles)
+        embedded_mol = embed_mol(mol, seed=1)
+
+        # Added Hs
+        assert Chem.MolToSmiles(embedded_mol) != Chem.MolToSmiles(mol)
+
+        with tempfile.NamedTemporaryFile(suffix='.pdb') as f:
+            write_embedded_mol_to_pdb(embedded_mol, ligand_pdb=f.name)
+            read_mol = read_mol_from_pdb(f.name)
+
+        # Hs are gone
+        assert Chem.MolToSmiles(mol) == Chem.MolToSmiles(read_mol)
+
+    def test_protonation(self):
+        mol = smiles_or_inchi_to_mol(lysine_smiles)
+        embedded_mol = embed_mol(mol, seed=1)
+
+        with tempfile.NamedTemporaryFile(suffix='.pdb') as pdb_file:
+            write_embedded_mol_to_pdb(embedded_mol, ligand_pdb=pdb_file.name)
+            protonate_pdb(pdb_file.name)
+
+            # Check that conversion to PDBQT doesn't break anything
+            with tempfile.NamedTemporaryFile(suffix='.pdbqt') as pdbqt_file:
+                convert_pdb_to_pdbqt(pdb_file.name, pdbqt_file.name)
+                convert_pdbqt_to_pdb(pdbqt_file.name, pdb_file.name)
+
+            read_mol = read_mol_from_pdb(pdb_file.name)
+
+        charges = tuple(sum(atom.GetFormalCharge() for atom in m.GetAtoms()) for m in (mol, read_mol))
+        assert charges == (0, 2)
+
+
+class TestRefinement:
+    def test_successful_refinement(self):
+        pass
+
+    def test_no_ff_parameters(self):
+        pass
+
+
 class TestDocking:
     def test_simple_docking(self):
         target = load_target('ABL1')
@@ -28,3 +90,19 @@ class TestDocking:
         smiles_2 = 'CC'
         energy_2, _ = target.dock(smiles_2)
         assert math.isclose(energy_2, -1.8)
+
+    def test_charged(self):
+        target = load_target('CYP3A4')
+        energy, aux = target.dock(lysine_smiles)
+        assert math.isclose(energy, -4.6)
+
+        charge = sum(atom.GetFormalCharge() for atom in aux['ligands'].GetAtoms())
+        assert charge == 2
+
+    # def test_difficult(self):
+    #     working_dir = '/home/gregor/.config/JetBrains/PyCharm2021.1/scratches/vina_tmp'
+    #     target = load_target('CYP3A4', working_dir=working_dir)
+    #     smiles = 'O=C1N(C=2N=C(OC)N=CC2N=C1C=3C=CC=CC3)C4CC4'
+    #
+    #     output = target.dock(smiles, verbose=True)
+    #     print(output)
