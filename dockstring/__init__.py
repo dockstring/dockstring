9a528da256e0747ee0d0908e4cf0c6ae767e038b
Gregor Simm <gncsimm@gmail.com>
2021-06-07 19:59:18 +0100

    add setup_logger to __init__.py

--- dockstring/__init__.py
+++ dockstring/__init__.py
@@ -1,3 +1,4 @@
 from .dockstring import load_target, list_all_target_names, DockingError
+from .utils import setup_logger
 
-__all__ = ['load_target', 'list_all_target_names', 'DockingError']
+__all__ = ['load_target', 'list_all_target_names', 'DockingError', 'setup_logger']
