ee1b4d99fdf56e4b06a0facae20191ef5b32c136
Miguel Garcia-Ortegon <mg770@cam.ac.uk>
2021-06-17 04:16:11 +0100

    Formatting and linting

--- scripts/compute_quality_metrics.py
+++ scripts/compute_quality_metrics.py
@@ -3,10 +3,12 @@ import pandas as pd
 import sklearn.metrics as metrics
 
 balanced_set = pd.read_csv(get_resources_dir() / 'data' / 'balanced_scores_1000_actives_1000_inactives.tsv', sep='\t')
-unbalanced_set = pd.read_csv(get_resources_dir() / 'data' / 'unbalanced_scores_200_actives_3800_inactives.tsv', sep='\t')
+unbalanced_set = pd.read_csv(get_resources_dir() / 'data' / 'unbalanced_scores_200_actives_3800_inactives.tsv',
+                             sep='\t')
 
-def enrichment_factor(labels,scores,top_k):
-    data = pd.DataFrame([labels,scores]).transpose().sort_values(by='score',ascending=False)
+
+def enrichment_factor(labels, scores, top_k):
+    data = pd.DataFrame([labels, scores]).transpose().sort_values(by='score', ascending=False)
     num_actives_before = int(sum(data['label']))
     num_actives_after = int(sum(data['label'].iloc[:top_k]))
     rate_before = num_actives_before / data.shape[0]
@@ -14,8 +16,9 @@ def enrichment_factor(labels,scores,top_k):
     ef = rate_after / rate_before
     return ef
 
+
 targets = balanced_set['target'].unique()
-quality_metrics = pd.DataFrame(index=targets,columns=['auc','ap','ef'])
+quality_metrics = pd.DataFrame(index=targets, columns=['auc', 'ap', 'ef'])
 
 for each_target in targets:
     # Load balanced and unbalanced sets
@@ -33,15 +36,14 @@ for each_target in targets:
     # Compute area under a ROC curve and average precision
     labels = each_balanced_set['label']
     scores = each_balanced_set['score']
-    auc = metrics.roc_auc_score(labels,scores)
+    auc = metrics.roc_auc_score(labels, scores)
     # Compute enrichment factor
     labels = each_unbalanced_set['label']
     scores = each_unbalanced_set['score']
-    ap = metrics.average_precision_score(labels,scores)
-    ef = enrichment_factor(labels=labels,scores=scores,top_k=200)
+    ap = metrics.average_precision_score(labels, scores)
+    ef = enrichment_factor(labels=labels, scores=scores, top_k=200)
     # Save to dataframe
-    quality_metrics.loc[each_target,['auc','ap','ef']] = [auc,ap,ef]
+    quality_metrics.loc[each_target, ['auc', 'ap', 'ef']] = [auc, ap, ef]
 
-
-quality_metrics = quality_metrics.sort_values(by='auc',ascending=False)
-quality_metrics.to_csv(get_resources_dir() / 'data' / 'quality_metrics.tsv',sep='\t',header=True,index=True)
+quality_metrics = quality_metrics.sort_values(by='auc', ascending=False)
+quality_metrics.to_csv(get_resources_dir() / 'data' / 'quality_metrics.tsv', sep='\t', header=True, index=True)
