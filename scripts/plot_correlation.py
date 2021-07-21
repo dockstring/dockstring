527861142d9a8741574581f3ceef2959306d5418
Gregor Simm <gncsimm@gmail.com>
2021-07-21 19:27:45 +0100

    update correlation plot

--- scripts/plot_correlation.py
+++ scripts/plot_correlation.py
@@ -7,28 +7,19 @@ from scipy import stats
 
 plt.rcParams.update({'font.size': 6})
 
-colors = [
-    '#1f77b4',  # muted blue
-    '#d62728',  # brick red
-    '#ff7f0e',  # safety orange
-    '#2ca02c',  # cooked asparagus green
-    '#9467bd',  # muted purple
-    '#8c564b',  # chestnut brown
-    '#e377c2',  # raspberry yogurt pink
-    '#7f7f7f',  # middle gray
-    '#bcbd22',  # curry yellow-green
-    '#17becf',  # blue-teal
-]
-
 
 def convert_dataset(dataset: pd.DataFrame) -> pd.DataFrame:
     dataset = dataset.dropna(axis='index')  # drop rows which contain missing values
     dataset = dataset.drop(labels=['inchikey', 'cluster', 'split', 'fp', 'label'], axis='columns').copy()
     dataset = dataset.set_index('smiles')
 
-    print('Warning: dropping rows with positive scores')
-    positive_score_row = (dataset > 0).any(axis=1)
-    return dataset.loc[~positive_score_row]
+    # print('Warning: dropping rows with positive scores')
+    # positive_score_row = (dataset > 0).any(axis=1)
+    # return dataset.loc[~positive_score_row]
+
+    print('Warning: setting positive scores to 0.0')
+    dataset[dataset > 0.0] = 0.0
+    return dataset
 
 
 def main() -> None:
@@ -42,24 +33,45 @@ def main() -> None:
         print(pd.Series(pearson_rs, name='pearson_r').sort_values())
 
     pairs = [('ADRB1', 'ADRB2'), ('MAOB', 'CYP3A4')]
+    offset = 0.75
 
     fig_size = 2.5
     num_cols = len(pairs)
     fig, axes = plt.subplots(nrows=1, ncols=num_cols, figsize=(num_cols * fig_size, fig_size), constrained_layout=True)
 
     for ax, (target_a, target_b) in zip(axes, pairs):
-        ax.scatter(dataset[target_a], dataset[target_b], color='black', s=7, alpha=0.75)
-        print(f'Correlation coefficient {target_a}, {target_b}: {pearson_rs[(target_a, target_b)]}')
+        lower = np.min(dataset[[target_a, target_b]].to_numpy())
+        upper = np.max(dataset[[target_a, target_b]].to_numpy())
+
+        hb = ax.hexbin(
+            dataset[target_a],
+            dataset[target_b],
+            gridsize=(15, 15),
+            extent=(lower - 2 * offset, upper + 2 * offset, lower - 2 * offset, upper + 2 * offset),
+            bins='log',
+            cmap='Blues',
+            linewidths=0.1,
+        )
+
+        ax.plot(
+            (lower - 2 * offset, upper + 2 * offset),
+            (lower - 2 * offset, upper + 2 * offset),
+            linestyle='dashed',
+            zorder=1,
+            color='black',
+            alpha=0.5,
+        )
+
+        ax.set_xlim(lower - offset, upper + offset)
+        ax.set_ylim(lower - offset, upper + offset)
 
         ax.set_xlabel(target_a)
         ax.set_ylabel(target_b)
 
-        # Plot XY line
-        limits = [
-            np.min([ax.get_xlim(), ax.get_ylim()]),  # min of both axes
-            np.max([ax.get_xlim(), ax.get_ylim()]),  # max of both axes
-        ]
-        ax.plot(limits, limits, linestyle='dashed', zorder=0, color=colors[0])
+        fig.colorbar(hb, ax=ax)
+        # cb.set_label('counts')
+
+        print(f'Correlation coefficient {target_a}, {target_b}: {pearson_rs[(target_a, target_b)]}')
 
     fig.savefig('correlation.pdf')
 
