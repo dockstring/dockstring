2af98b1d3305a89c3aae8958a4e7204b0b23ef09
Gregor Simm <gncsimm@gmail.com>
2021-09-28 16:01:32 +0100

    optimize imports

--- dockstring/__init__.py
+++ dockstring/__init__.py
@@ -1,7 +1,7 @@
-from .target import load_target, list_all_target_names
 from .errors import (DockstringError, CanonicalizationError, ParsingError, OutputError, SanityError, EmbeddingError,
                      StructureOptimizationError, FormatConversionError, ProtonationError, PoseProcessingError,
                      VinaError, DockingError)
+from .target import load_target, list_all_target_names
 from .utils import setup_logger
 
 __all__ = [
