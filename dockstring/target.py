aec26ca4a294c63ce485a96f6c7ca81de3d36865
Austin Tripp <ajt212@cam.ac.uk>
2022-10-30 16:21:16 +0000

    Add warning for rdkit version.

--- dockstring/target.py
+++ dockstring/target.py
@@ -15,7 +15,7 @@ from .utils import (smiles_to_mol, embed_mol, refine_mol_with_ff, convert_pdbqt_
                     parse_affinities_from_output, parse_search_box_conf, PathType, get_targets_dir, get_vina_path,
                     get_resources_dir, check_mol, canonicalize_smiles, verify_docked_ligand, check_vina_output,
                     assign_stereochemistry, assign_bond_orders, sanitize_mol, protonate_mol, write_mol_to_mol_file,
-                    convert_mol_file_to_pdbqt, check_charges, check_obabel_install)
+                    convert_mol_file_to_pdbqt, check_charges, check_obabel_install, check_rdkit_version)
 
 
 def load_target(name: str, *args, **kwargs) -> 'Target':
@@ -173,6 +173,9 @@ class Target:
 
         # Check that the right Open Babel version is available
         check_obabel_install()
+
+        # Check rdkit version
+        check_rdkit_version()
 
         # Protonate ligand
         protonated_mol = protonate_mol(mol, pH=pH)
