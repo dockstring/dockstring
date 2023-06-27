3ab1faf32c49fe9ba043181e0ee83b0b158ee86e
Austin Tripp <ajt212@cam.ac.uk>
2023-06-27 16:40:35 +0100

    Revise tests for dataset matching, including moving code to download the dataset into the main package.

--- tutorials/simple_example.py
+++ tutorials/simple_example.py
@@ -5,4 +5,4 @@ from dockstring import load_target
 if __name__ == "__main__":
     target = load_target("DRD2")
     score, _ = target.dock("CC1=C(C(=O)N2CCCCC2=N1)CCN3CCC(CC3)C4=NOC5=C4C=CC(=C5)F")
-    print(f"Docking was successful, score={score:.3g}. END OF SCRIPT.")
+    print(f"Docking completed without error, score={score:.3g}. END OF SCRIPT.")
