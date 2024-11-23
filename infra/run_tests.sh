49d51128f38b607a7604acc8d17e85db80c9a8b8
Gregor Simm <gncsimm@gmail.com>
2024-11-23 23:08:10 +0000

    save

--- infra/run_tests.sh
+++ infra/run_tests.sh
@@ -2,6 +2,6 @@
 
 docker run -it --rm \
     --mount "type=bind,source=$(pwd),target=/dockstring" \
-    docker.io/library/dockstring:latest \
-    bash -c "conda activate dockstring && pip install /dockstring pytest && pytest /dockstring --collect-only"
+    dockstring:latest \
+    bash -c "conda activate dockstring && pip install /dockstring pytest && pytest /dockstring"
     
