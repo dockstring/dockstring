8c5394a007cbaff70f982f41b93c5a0a08c93a54
Gregor Simm <gncsimm@gmail.com>
2024-11-30 13:52:29 +0000

    silence pyright warning

--- dockstring/__init__.py
+++ dockstring/__init__.py
@@ -1,6 +1,6 @@
 try:
     # Python >=3.8
-    from importlib.metadata import PackageNotFoundError, version
+    from importlib.metadata import PackageNotFoundError, version  # pyright: ignore[reportMissingImports]
 except ModuleNotFoundError:
     # Python 3.7
     from importlib_metadata import PackageNotFoundError, version  # type: ignore[no-redef]
